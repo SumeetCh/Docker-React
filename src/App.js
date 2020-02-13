c9fde34c11eeb079e2ae68f9f040bd3c5d169331
Sumeet Chawla <01.sumeetchawla@gmail.com>
2020-02-13 17:38:40 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         The JS is changing
         </p>
         <a
           className="App-link"
