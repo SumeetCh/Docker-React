be81ecb53d0a238b78b9cc59b7920b15d02efd62
Sumeet Chawla <01.sumeetchawla@gmail.com>
2020-02-18 13:52:17 -0500

    Changed JS File

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Test JS 1
+         Welcome Home
         </p>
         <a
           className="App-link"
