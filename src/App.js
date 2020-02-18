e495c67db10ee86f254b8ace4ed86407cacbe440
Sumeet Chawla <01.sumeetchawla@gmail.com>
2020-02-18 12:41:28 -0500

    added text to JS

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         The JS is changing
+         Test JS
         </p>
         <a
           className="App-link"
